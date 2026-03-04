bcad53387f6a00d3105218f542170395f2156ef7
Revvcode <chriskros387@gmail.com>
2026-03-04 19:59:13 +0530

    CS50P Lecture-8 OOPS going on

--- python-core/student.py
+++ python-core/student.py
@@ -1,12 +1,25 @@
 #you can return a list or dictionary based on need
+#classes help you define a blueprint to create objects, class is bascially a custom data type
+# raise helps make our own errors
+import re
+class Student:
+    def __init__(self,name,house):
+        if not name: # literally if name == ""
+            raise ValueError("Missing name")
+        if (house not in ["Gryffindor","Hufflepuff","Ravenclaw","Slytherin"]):
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+
 def main():
     student = get_student()
-    print(f"{student[0]} from {student[1]}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return [name, house]
+    return Student(name,house)
+
 
 if __name__ == "__main__":
     main()
