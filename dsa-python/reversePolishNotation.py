c1e0c23d993858b057a139f77a9b193f1b516117
Revvcode <chriskros387@gmail.com>
2026-04-25 19:48:26 +0530

    DSA Reverse Polish Notation

---
+++ dsa-python/reversePolishNotation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack = []
+
+        for t in tokens:
+            if t not in "+-*/":
+                stack.append(int(t))
+            else:
+                r = stack.pop()
+                l = stack.pop()
+
+                if t == '+':
+                    stack.append(l + r)
+                elif t == '-':
+                    stack.append(l - r)
+                elif t == '*':
+                    stack.append(l * r)
+                else:
+                    stack.append(int(float(l) / r))  
+
+        return stack[0]
